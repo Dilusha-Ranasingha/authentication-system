95efa7a8b899cd09f783d6fe5f67bb3ec0407627
Dilusha-Ranasingha <dilushar10@gmail.com>
2024-12-26 14:39:32 +0530

    chore: remove index.route.js file

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,15 @@ import express from 'express';         //A framework to create APIs easily.
 import dotenv from 'dotenv';         //To load environment variables
 import { connectDB } from './config/connectDB.js';
 import authRoutes from './routes/auth.route.js';    //Imports the authRoutes from the auth.route.js file.
-import indexRoutes from './routes/index.route.js';  //Imports the indexRoutes from the index.route.js file.
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5001;   //Sets the port to 5000 if the PORT environment variable is not set.
 
 
-app.use('/', indexRoutes);        //Tells the server to use the indexRoutes when the URL has / in it.
 app.use('/api/auth' , authRoutes);     //Tells the server to use the authRoutes when the URL has /api/auth in it.
 
-app.listen(3000, ()=>{            //Starts the server on port 3000.
-    console.log('Server is running on port 3000');
+app.listen(PORT, ()=>{            //Starts the server on port 3000.
+    console.log('Server is running on port:', PORT);
     connectDB();                 //Calls connectDB() when the server starts to connect to MongoDB using your .env file.
 })
