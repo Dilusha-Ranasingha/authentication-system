fb971befc5e7a4209b027ea5ce3da02f6aefdc97
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-01-03 23:56:55 +0530

    chore: make the frontend setup for implement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import FlotingShape from "./components/flotingShape";
+
 function App() {
 
   return (
-    <>
-      <h1 class='text-red-500 text-5xl'>hellow world</h1>
-    </>
+    <div 
+      className='min-h-screen bg-gradient-to-br from-gray-400 to-blue-900 to-emarald-900 flex item-center justify-center relative overflow-hidden'>
+      <FlotingShape color="bg-blue-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
+      <FlotingShape color="bg-blue-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
+      <FlotingShape color="bg-blue-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+
+    </div>
   )
 }
 
-export default App
+export default App;
