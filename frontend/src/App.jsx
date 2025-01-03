e2b550639d4506ec3afb4fbf724f2ac753b545ce
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-01-04 00:11:09 +0530

    chore: make the Routes  for signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import FlotingShape from "./components/flotingShape";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
 
@@ -8,6 +11,13 @@ function App() {
       <FlotingShape color="bg-blue-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
       <FlotingShape color="bg-blue-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
       <FlotingShape color="bg-blue-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+
+
+      <Routes>
+        <Route path='/' element={"Home"} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
 
     </div>
   )
