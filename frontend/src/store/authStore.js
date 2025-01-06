dac6eca5e26e7c25a529908c1d100362ce1ca043
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-01-06 23:00:54 +0530

    chore: (store)make the folder and file to manage the state of the user authentication

---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,3 @@
+//this file is use to manage the state of the user authentication
+import { create } from "zustand";          //this is a state management library
+import axios from "axios";                 //this is a axios library to make http request
